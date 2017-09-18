0d50ffe8baddefe5fd8ac14a1851a3527b3963ea
HashiCorp Training <training@hashicorp.com>
2017-09-18 21:05:26 +0000

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,66 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your AMI ID is:
+#
+#     ami-1d4e7a66
+#
+# Your subnet ID is:
+#
+#     subnet-77f9d52d
+#
+# Your security group ID is:
+#
+#     sg-e72efe94
+#
+# Your Identity is:
+#
+#     terraform-hound
+#
+
+
+terraform {
+  backend "atlas" {
+    name = "chrisatconcur/training"
+   }
+}
+provider "dnsimple" {
+  token = "awesometoken"
+  account = "awesomeaccount"
+}
+
+resource "dnsimple_record" "awesomednsrecord" {
+  domain = "www.awesomednsrecord.com" 
+  name = "awesomednsrecord"
+  value = "${aws_instance.web.0.public_ip}"
+  type = "A"
+  ttl = 3600
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+resource "aws_instance" "web" {
+  count                  = 2
+  ami                    = "ami-1d4e7a66"
+  instance_type          = "t2.micro"
+  subnet_id              = "subnet-77f9d52d"
+  vpc_security_group_ids = ["sg-e72efe94"]
+
+  tags {
+    Identity = "terraform-hound"
+    Name = "web ${count.index}"
+    Foo = "bar"
+  }
+}
+
+
+output "web_public_ip" {
+  value = "${aws_instance.web.*.public_ip}"
+}
+output "web_public_dns" {
+  value = "${aws_instance.web.*.public_dns}"
+}
